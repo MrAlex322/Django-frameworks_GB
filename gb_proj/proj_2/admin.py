5455c44b19b7237941e7013c2d612d36559d4b8a
alex1331 <danilin999@bk.ru>
2024-02-26 09:46:03 +0300

    Настройте под свои нужды вывод информации о клиентах, товарах и заказах на страницах вывода информации об объекте и вывода списка объектов.

--- gb_proj/proj_2/admin.py
+++ gb_proj/proj_2/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Client, Product, Order
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'address', 'registration_date')
+    search_fields = ('name', 'email')
+    list_filter = ('registration_date',)  # Фильтр по дате регистрации
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'added_date')
+    search_fields = ('name',)
+    list_filter = ('added_date',)  # Фильтр по дате добавления
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'total_amount', 'order_date')
+    search_fields = ('client__name', 'total_amount')
+    list_filter = ('order_date', 'client')  # Фильтр по дате заказа и по клиенту
